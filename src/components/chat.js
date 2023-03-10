4f55cacfae51485ce7b1b9d8dd593bd2a781d9ba
3mpireGamer <3mpiregamer@gmail.com>
2023-03-10 12:28:47 -0600

    Chat modal visuals based on case status

--- src/components/chat.js
+++ src/components/chat.js
@@ -45,18 +45,23 @@ export function Chat({ openedTicket, openTicket, refresh, toggleRefresh }) {
    
    if (selectedTicket.messages) { return (
       <Stack direction="column" justifyContent="flex-end" alignItems="center" spacing={2} width='400px'>
-         <MessagingHead confirm={confirm} setConfirm={setConfirm} closeTicket={closeTicket} selectedTicket={selectedTicket} />
+         <MessagingHead status={selectedTicket.status} confirm={confirm} setConfirm={setConfirm} closeTicket={closeTicket} selectedTicket={selectedTicket} />
          <Messages ticket={selectedTicket} />
-         <MessageBox handleNewMessage={handleNewMessage} selectedTicket={selectedTicket} />
+         <MessageBox status={selectedTicket.status} handleNewMessage={handleNewMessage} selectedTicket={selectedTicket} />
       </Stack> 
    )}
    return <></>
 }
-function MessagingHead({ confirm, setConfirm, closeTicket, selectedTicket }) {
-   return (<Stack direction="row" justifyContent="space-between" alignItems="center" spacing={1} width='100%'>
+function MessagingHead({ status, confirm, setConfirm, closeTicket, selectedTicket }) {
+   return status !== 'Closed' ? (<Stack direction="row" justifyContent="space-between" alignItems="center" spacing={1} width='100%'>
    <ChatBubbleOutlineIcon fontSize='large' />
    <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={1}>
       <TicketCloser confirm={confirm} setConfirm={setConfirm} closeTicket={closeTicket} selectedTicket={selectedTicket} />
+   </Stack></Stack>
+   ) : (<Stack direction="row" justifyContent="space-between" alignItems="center" spacing={1} width='100%'>
+   <ChatBubbleOutlineIcon fontSize='large' />
+   <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={1}>
+      <Button variant='text' size='large'>Case {selectedTicket.caseno}</Button>
    </Stack></Stack>
 )}
 function TicketCloser({ confirm, setConfirm, closeTicket, selectedTicket }) {
@@ -95,16 +100,16 @@ function Message({ head, content, align }) {
       <Typography textAlign={align}>{content}</Typography>
    </Box>
 )}
-function MessageBox({ handleNewMessage, selectedTicket }) {
+function MessageBox({ status, handleNewMessage, selectedTicket }) {
    const [label, setLabel] = useState('Send Message');
-   return (
+   return status !== 'Closed' ? (
    <TextField id='content' variant='outlined' label={label}
       fullWidth multiline maxRows={3} minRows={3}
       sx={{backgroundColor: 'secondary.main', borderRadius: '4px'}} 
       onKeyDown={(e) => {if (e.key === 'Enter') {handleNewMessage(selectedTicket, e)}}}
       onFocus={() => {setLabel('Press Enter to Send Message')}} onBlur={() => {setLabel('Send Message')}}
-   />
-)}
+   /> ) : <Typography>Ticket is Closed</Typography>
+}
 
 function formatDate(date) {
    return parseMonth(date.getMonth()) + ' ' + date.getDate() + ' - ' + date.getHours() + ':' + date.getMinutes()
