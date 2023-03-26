48a4fb38dd254ca38162d8a3207e597a9f0f7f96
3mpireGamer <3mpiregamer@gmail.com>
2023-03-26 10:38:22 -0500

    Slight improvement to scroll modal on mobile

--- src/modals/scrollmodal.js
+++ src/modals/scrollmodal.js
@@ -4,31 +4,37 @@ import { IconButton } from "@mui/material";
 import { Box } from "@mui/system";
 import { elementWidth } from "../utils";
 
-const sx = {
+const bottomSx = {
    position: 'fixed', bottom: '0px', left: '0px', width: 'fit-content'
 }
 
 export function ScrollModal({ openedTicket }) {
+   const [sx, setSx] = useState(bottomSx);
    const [show, setShow] = useState(!openedTicket || window.innerWidth > elementWidth)
    const modalWidth = useRef();
    const scrollModal = useRef();
 
    const handleResize = useCallback(() => {
       if (scrollModal.current) modalWidth.current = scrollModal.current.clientWidth
-      console.log(scrollModal.current)
       setShow(!openedTicket || window.innerWidth > elementWidth + modalWidth.current)
    }, [openedTicket])
+   const handleScroll = useCallback(() => setSx(bottomSx), [])
 
    useEffect(() => {
       window.addEventListener('resize', handleResize)
-      return () => window.removeEventListener('resize', handleResize)
-   }, [handleResize])
+      window.addEventListener('scroll', handleScroll)
+      return () => {
+         window.removeEventListener('resize', handleResize)
+         window.removeEventListener('resize', handleScroll)
+      }
+   }, [handleResize, handleScroll])
 
    //console.log(show)
    return (show ? 
    <Box width='0px'><Box p={2} sx={sx} ref={scrollModal}>
-      <IconButton sx={{border: 1, borderColor: 'rgba(0, 0, 0, 0.27)', borderRadius: 1, backgroundColor: 'secondary.dark'}} onClick={() => {
-         document.getElementById('head').scrollIntoView({behavior: 'smooth'});
-      }}><KeyboardArrowUpIcon color='black' /></IconButton>
+      <Box sx={{border: 1, borderColor: 'rgba(0, 0, 0, 0.27)', borderRadius: 1, backgroundColor: 'secondary.dark'}}>
+         <IconButton onClick={() => {document.getElementById('head').scrollIntoView({behavior: 'smooth'});}}>
+         <KeyboardArrowUpIcon color='black' /></IconButton>
+      </Box>
    </Box></Box> : <></>
 )}
